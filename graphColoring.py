af5757d9e90ca193e7fb929e13315d2dfa4e3298
Alfred Leong <83620606+MakiseKurisu-004@users.noreply.github.com>
2021-08-27 03:20:31 -0400

    Update graphColoring.py

--- graphColoring.py
+++ graphColoring.py
@@ -1,4 +1,3 @@
 from Graph import Graph
-
 graph = Graph(int(input("Number of nodes: ")),list(map(str, input("Format: 23 if node 2 is connected to node 3\nEnter the edges: ").split())))
 graph.doAll()
