b7b38cf45feae4efacd3b5b6d323308082be5ff2
Alfred Leong <83620606+MakiseKurisu-004@users.noreply.github.com>
2021-08-27 23:26:50 -0400

    Update graphColoring.py

--- graphColoring.py
+++ graphColoring.py
@@ -1,3 +1,4 @@
 from Graph import Graph
 graph = Graph(int(input("Number of nodes: ")),list(map(str, input("Format: 23 if node 2 is connected to node 3\nEnter the edges: ").split())))
-
+# print(graph.coveringTable)
+# print(graph.pos)
